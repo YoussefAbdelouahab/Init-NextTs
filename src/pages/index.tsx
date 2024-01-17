a725f7c12c348806397d6bdf53b0e10f5fccd6eb
Yousef <abdelo_y@etna-alternance.net>
2024-01-17 12:11:53 +0100

    add init next project

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Head from 'next/head';
+import "./home.css"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App </title>
+          < link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Home Page</h1>
+      <h2>NextJs Project initialised with typescript</h2>
+    </>
+    );
+}
